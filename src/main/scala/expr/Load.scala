33ee205c52d0f8e91b6aab575e65f633a17d68fb
cascala <cascala@users.noreply.github.com>
2016-04-10 15:23:06 -0400

    Allow file inputs - closes issue #7

--- src/main/scala/expr/Load.scala
+++ src/main/scala/expr/Load.scala
@@ -4,6 +4,8 @@ import galileo.environment.Environment
 import galileo.exprhandler.ExprHandler
 import galileo.parser._
 
+import scala.io.Source
+
 //import scala.util.parsing.combinator.JavaTokenParsers
 //import scala.util.parsing.combinator.ImplicitConversions // really useful...
 //import java.io.FileNotFoundException
@@ -13,8 +15,10 @@ import galileo.parser._
 
 case class Load(filename:String) extends Expr {
 	def info(env:Option[Environment]=None) = "Load(" + filename + ")"
+	
 	def expressions:List[Expr] = {
-		var contents = "a=10;b=11;c=a+b;c"
+		//var contents = "a=10;b=11;c=a+b;c"
+		val contents = Source.fromFile(filename).getLines.mkString
 		// parse this (or should we send the contents to the command window?)
 		val p = new Parser()
 		p.parse( contents ) match {
@@ -30,13 +34,16 @@ case class Load(filename:String) extends Expr {
 			case err => List( ErrorExpr( "Failure to parse " + contents + err  ) )
 		}
 	}
+	
+
 	override def visit( env:Option[Environment] ) = {
 		var p = new Parser()
 		//var h = new ExprHandler()
 		
 		//import parser.NoSuccess // this does not compile... argh
 //import p.Success
-		var contents = "a=10;b=11;c=a+b"
+		//var contents = "a=10;b=11;c=a+b"
+		val contents = Source.fromFile(filename).getLines.mkString
 		// parse this (or should we send the contents to the command window?)
 		p.parse( contents ) match {
 			case p.Success(expressions,_) => { 
