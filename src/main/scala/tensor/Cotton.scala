19df0f9a2afdfb7be067681664f241cb4d946f07
cascala <cascala@users.noreply.github.com>
2016-10-09 18:08:25 -0400

    Added files

---
+++ src/main/scala/tensor/Cotton.scala
@@ -0,0 +1,16 @@
+package galileo.tensor
+
+import galileo.expr.{Diff,Expr,Fraction,Number,Product}
+
+// Schouten Tensor
+object CottonTensor{
+	def apply(metric:Metric) = { 
+		require( metric.dimension > 2 )
+		( RicciTensor(metric) - ( metric.toLower * RicciScalar( metric ) / Number( 2 * ( metric.dimension - 1 ) ) ) ) / Number( metric.dimension - 2 )
+	}
+}
+
+// u for unhandled, so expr has not been visit-ed yet
+case class CottonTensorU(expr:Expr) extends TensorU {
+	val generator:Metric=>Tensor = CottonTensor.apply
+}
--- src/main/scala/tensor/LeviCevita.scala
+++ src/main/scala/tensor/LeviCevita.scala
@@ -5,8 +5,11 @@ import galileo.expr.{Expr,Fraction,Number,Product,Sum}
 // Levi-Cevita Tensor
 object LeviCevita {
 	def apply(indices:TensorIndex*) = { 
+
 		def lcValue( l:List[Int] ) = l match {
-			
+			case a :: b :: c :: Nil if ( a == b ) => Number( 0 )
+			case a :: b :: c :: Nil if ( b == c ) => Number( 0 )
+			// todo
 			case _ => Number( 1 ) // todo: fix me
 
 		}
