4d02aa28c4fabb1626e6405d5749a9066ad88380
cascala <cascala@users.noreply.github.com>
2018-04-09 14:46:05 -0400

    Exposed visit function Expr -> Expr

--- src/main/scala/ExprHandler.scala
+++ src/main/scala/ExprHandler.scala
@@ -16,19 +16,24 @@ import galileo.trigonometry._
 // I need to add an explicit eval step (replacing variables etc)
 class ExprHandler{
   def eval(env:Environment, expr:Expr):String = {
-  	def visit(expr:Expr):Expr = { /*println( expr );*/ expr match {
-      case Eval( e ) => visit( e ).eval()
+    expr match {
+      case Who() => env.toString
+      case Whos() => env.toString
+      case Clear() => { env.clear; "" }
+      case s:SystemCommand => s.toString()
+      case Exit() => { System.exit( 0 ); "" } // caught in Shell, rethrown there
+      case e:Expr => { val ans = visit( env, e ); env.set( "ans", ans ); ans.toString }
+    }
+  }
+
+  def visit( env:Environment, e:Expr):Expr = { 
+    e match {
+      case Eval( e ) => visit( env, e ).eval()
   	
       case MatInv( MatInv( a ) ) => a.visit( Some( env ) ) // should in theory check for singular matrix here
       case m:MatInv  => m.visit( Some( env ) )
       case n:MatNorm => n.visit( Some( env ) )
 
-/*
-  		case MatLU( e )=> visit ( e ) match{
-  			case m:Matrix => m.l_lup
-  			case _ => ErrorExpr( "lu(matrix) can only be applied to a matrix")
-  		}
-*/
       case d:Derivative => d.visit( Some( env ) )
   		case Number( v ) => Number( v ) // can't simplify that...
       //case NumberI( v ) => NumberI( v ) // or that
@@ -43,7 +48,7 @@ class ExprHandler{
       case r:Rand => r.visit( Some( env ) )
       case l:Load => l.visit( Some( env ) )
 
-  		case Assignment2( n0, n1, MatLU( e ) ) => visit( e ) match {
+  		case Assignment2( n0, n1, MatLU( e ) ) => visit( env, e ) match {
   			case m:Matrix => m.lup_lup match{
 				  case (l,u,p) => {
             val pil = p.solve( l.toDenseMatrix ) // inv(P)*L // would be more efficient to work with rows of L?!?!
@@ -58,7 +63,7 @@ class ExprHandler{
   			case _ => ErrorExpr( "function 'lu(matrix)' can only be applied to a matrix" )
   		}
       case a:Assignment2 => ErrorExpr( "right hand side can not produce two variables" )
-  		case Assignment3( n0, n1, n2, MatLU( e ) ) => visit( e ) match {
+  		case Assignment3( n0, n1, n2, MatLU( e ) ) => visit( env, e ) match {
   			case m:Matrix => m.lup_lup match {
           case (l,u,p) => {
             env.set( n0, l )
@@ -78,28 +83,20 @@ class ExprHandler{
 
   		
   		case Assignment1( name, value ) => { 
-				val vv = visit( value ) 
+				val vv = visit( env, value ) 
 				env.set( name, vv )
 				vv 
 			}
   		
-
   		case p:Power => p.visit( Some( env ) )
 
-  		//case galileo.linalg.Vector( elements ) => galileo.linalg.Vector( elements.map( elem => visit( elem ) ) )
   		case m:Matrix => m.visit( Some( env ) ) //Matrix( ll.map( row => row.map( elem => visit( elem ) ) ) )
+    
       case u:OnesMatrixU => u.visit( Some( env ) )
-  		//case d:DiagMatrix => d.visit( Some( env ) )
-  		//case o:OnesMatrix => o.visit( Some( env ) )
-
-
+  	
   		case b:Bool => b
-  		// All the commands
-  		//case Who() => expr // Maybe make it return None, and handle below?
-  		//case Whos() => expr
-  		//case _ => None
-  		//case t:ExprF1(e) => visit( e )
-  		case BoolAnd( e,f ) => (visit( e ), visit( f ) ) match {
+  	
+    	case BoolAnd( e,f ) => ( visit( env, e ), visit( env, f ) ) match {
   			case ( l:Bool, r:Bool) => l & r
   			case ( l:Bool, r:Number ) => l & r
   			case ( l:Number, r:Bool ) => l & r
@@ -107,7 +104,7 @@ class ExprHandler{
    			case ( l:Expr, r:Expr ) => BoolAnd( l, r ) //throw new Exception( "Not a boolean")
   		}
 
-  		case BoolOr( e,f ) => (visit( e ), visit( f ) ) match {
+  		case BoolOr( e,f ) => (visit( env, e ), visit( env, f ) ) match {
   			case ( l:Bool, r:Bool) => l | r
   			case ( l:Bool, r:Number ) => l | r
   			case ( l:Number, r:Bool ) => l | r
@@ -115,7 +112,7 @@ class ExprHandler{
    			case ( l:Expr, r:Expr ) => BoolOr( l, r ) //throw new Exception( "Not a boolean")
   		}
 
-  		case BoolAndSc( e,f ) => (visit( e ), visit( f ) ) match {
+  		case BoolAndSc( e,f ) => (visit( env, e ), visit( env, f ) ) match {
   			case ( l:Bool, r:Bool) => l && r
   			case ( l:Bool, r:Number ) => l && r
   			case ( l:Number, r:Bool ) => l && r
@@ -123,7 +120,7 @@ class ExprHandler{
    			case ( l:Expr, r:Expr ) => { println( "l.info" + l.info() + ", r.info():" + r.info() ); BoolAndSc( l, r ) } //throw new Exception( "Not a boolean")
   		}
 
-  		case BoolOrSc( e,f ) => (visit( e ), visit( f ) ) match {
+  		case BoolOrSc( e,f ) => (visit( env, e ), visit( env, f ) ) match {
   			case ( l:Bool, r:Bool) => l || r
   			case ( l:Bool, r:Number ) => l || r
   			case ( l:Number, r:Bool ) => l || r
@@ -131,7 +128,7 @@ class ExprHandler{
    			case ( l:Expr, r:Expr ) => BoolOrSc( l, r ) //throw new Exception( "Not a boolean")
   		}
 
-  		case BoolXor( e,f ) => (visit( e ), visit( f ) ) match {
+  		case BoolXor( e,f ) => ( visit( env, e ), visit( env, f ) ) match {
   			case ( l:Bool, r:Bool) => l ^ r
   			case ( l:Bool, r:Number ) => l ^ r
   			case ( l:Number, r:Bool ) => l ^ r
@@ -139,7 +136,7 @@ class ExprHandler{
    			case ( l:Expr, r:Expr ) => BoolXor( l, r ) //throw new Exception( "Not a boolean")
   		}
 
-  		case BoolNot( e ) => visit( e ) match {
+  		case BoolNot( e ) => visit( env, e ) match {
   			case l:Bool => !l
   			case l:Number => !l
    			case l:Expr => BoolNot( l ) //, r ) //throw new Exception( "Not a boolean")
@@ -149,9 +146,9 @@ class ExprHandler{
   		//case CosF1( AcosF1( e ) ) => visit( e )
   		//case SinF1( AsinF1( e ) ) => visit( e )
   		//case TanF1( AtanF1( e ) ) => visit( e )
-		  case AcosF1( CosF1( e ) ) => visit( e )
-  		case AsinF1( SinF1( e ) ) => visit( e )
-  		case AtanF1( TanF1( e ) ) => visit( e )
+		  case AcosF1( CosF1( e ) ) => visit( env, e )
+  		case AsinF1( SinF1( e ) ) => visit( env, e )
+  		case AtanF1( TanF1( e ) ) => visit( env, e )
 
 		  /*case CosF1( Number( v ) ) => Number( math.cos( v ) )
   		case SinF1( Number( v ) ) => Number( math.sin( v ) )
@@ -166,35 +163,16 @@ class ExprHandler{
   		case as:AsinF1 => as.visit( Some( env ) )
   		case at:AtanF1 => at.visit( Some( env ) )
 
-  		/*
-  		case TrigF1( _:Expr ) => t.e match {
-  			case ConstantPi() => t.eval( e ) 
-  			case _:Expr => visit( _ ) match {
-  				case l:Bool => Number( expr.eval( l ) )
-  				case Number( n ) => Number( expr.eval( n ) )
-  				case _ => expr 
-  			}
-  		}
-  		*/
-  		case j:ConstantJ => visit( j.value )
+  		case j:ConstantJ => visit( env, j.value )
   		case c:Constant => c // don't expand down for pi and e
       case LogF1( c:ConstantE ) => Number( 1 )
-      case ExpF1( LogF1( e ) ) => visit( e )
-      case LogF1( ExpF1( e ) ) => visit( e )
+      case ExpF1( LogF1( e ) ) => visit( env, e )
+      case LogF1( ExpF1( e ) ) => visit( env, e )
       case l:LogF1 => l.visit( Some( env ) )
       case l:ExpF1 => l.visit( Some( env ) )
   		//case c:Constant => visit( c.value ) // don't expand down for pi and e
 
       case whatever => whatever.visit( Some( env ) )
-  	} }
-
-    expr match {
-  		case Who() => env.toString
-  		case Whos() => env.toString
-  		case Clear() => { env.clear; "" }
-      case s:SystemCommand => s.toString()
-  		case Exit() => { System.exit( 0 ); "" } // caught in Shell, rethrown there
-  		case e:Expr => { val ans = visit( e ); env.set( "ans", ans ); ans.toString }
-  	}
-  }
-}
+    } // match
+  } // visit
+} // class ExprHandler
